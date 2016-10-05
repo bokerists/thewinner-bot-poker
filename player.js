81e5bd5f1cafd064c618c1dff4e6319307368392
Giorgio Aquino <gigetto@gmail.com>
2016-10-05 18:36:26 +0200

    setup bot player

---
+++ player.js
@@ -0,0 +1,30 @@
+
+exports = module.exports = {
+
+  VERSION: 'Superstar poker js-player',
+
+  bet: function (gamestate) {
+
+    //
+    // gamestate contains info about the state of the game.
+    // check the documentation at https://bot-poker.herokuapp.com/wiki#gamestate for further info about the data structure.
+
+    //
+    // you just have to return the amount that you want to bet.
+
+
+
+    // enjoy the game!
+
+    //
+    // currently we just fold every single hand.
+
+    'use strict';
+
+    console.log(`Currently playing tournament ${gamestate.tournamentId}`);
+
+    return 0;
+
+  }
+
+};
